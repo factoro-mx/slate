81807e6a1949f01ffd869e329a83cd5381d117c0
Fer Bueno <ferbuenob@gmail.com>
2025-03-20 16:03:53 -0600

    migrates to new slate version

--- config.rb
+++ config.rb
@@ -57,6 +57,7 @@ end
 # Deploy Configuration
 # If you want Middleman to listen on a different port, you can set that below
 set :port, 4567
+set :url, 'https://api.factoro.mx'
 
 helpers do
   require './lib/toc_data.rb'
